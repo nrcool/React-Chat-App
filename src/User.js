51d1c425c23f913cdaac630802516dfee4d03a10
nrcool <nomanalam7214@gmail.com>
2021-11-20 16:57:26 +0100

    profile component

---
+++ src/User.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { MyContext } from './context/MyConext'
+import "./User.css"
+export default function User() {
+    const {user}=useContext(MyContext)
+    return (
+        <div className="container d-flex justify-content-center align-items-center">
+        <div className="card1">
+            <div className="upper"> <img src="https://i.imgur.com/Qtrsrk5.jpg" className="img-fluid"/> </div>
+            <div className="user text-center">
+                <div className="profile"> <img src={user.photoURL} className="rounded-circle" width="80"/> </div>
+            </div>
+            <div className="mt-5 text-center">
+                <h4 className="mb-0">{user.displayName}</h4> <span className="text-muted d-block mb-2">{user.location}</span> <button className="btn btn-primary btn-sm follow">{user.email}</button>
+                <div className="d-flex justify-content-between align-items-center mt-4 px-4">
+                    <div className="stats">
+                        <h6 className="mb-0">sent messages</h6> <span>812</span>
+                    </div>
+                    <div className="stats">
+                        <h6 className="mb-0">uploaded files</h6> <span>12</span>
+                    </div>
+                    <div className="stats">
+                        <button className="btn btn-primary">LogOut</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
