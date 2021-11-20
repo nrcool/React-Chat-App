9a3144abde41bf1e455b3685dbbb706d6aa42e47
nrcool <nomanalam7214@gmail.com>
2021-11-20 21:38:34 +0100

    online  and offline users

---
+++ src/Users.js
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect } from "react";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  getFirestore,
+  onSnapshot,
+  Timestamp,
+  orderBy,
+  query,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+import "./Users.css"
+import { MyContext } from "./context/MyConext";
+const db = getFirestore();
+
+export default function Users() {
+  const { users, setUsers,setOnlineUsers,onlineUsers } = useContext(MyContext);
+
+  useEffect(() => {
+    const docRef = collection(db, "users");
+    const q = query(docRef, orderBy("online"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const users = snapshot.docs.map((doc) => {
+        return doc.data();
+      });
+      let onlineU=users.filter(user=>user.online)
+      let offlineU=users.filter(user=>!user.online)
+      setUsers(offlineU);
+      setOnlineUsers(onlineU)
+    });
+    return unsubscribe;
+  }, []);
+
+  return <div className="allusers">
+      <h4>Online Users</h4>
+      <div className="online-users">
+          {onlineUsers.map(user=>{
+      return(
+          <div className="online-user">
+          <img src={user.photoURL}  className="online-user-image" alt="" />
+          <p>{user.displayName}</p>
+          </div>
+      )
+  })
+  
+  }
+      </div>
+      <h4>Offline Users</h4>
+      <div className="offline-users">
+          {users.map(user=>{
+              return(
+                  <div className="offline-user">
+                      <img src={user.photoURL} className="offline-user-image"  alt="" />
+                         <p>{user.displayName}</p> 
+                      </div>
+             
+              )
+          })}
+      </div>
+      </div>;
+}
